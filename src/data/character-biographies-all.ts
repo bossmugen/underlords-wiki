ae85e7fa3cb6315739309844b608b5b791a86906
bossmugen <zhenrammsteiner@gmail.com>
2026-09-01 03:06:09 -0700

    wiki: activate Run 126 character deepening

--- src/data/character-biographies-all.ts
+++ src/data/character-biographies-all.ts
@@ -79,6 +79,7 @@ import * as run79WallModule from "./character-biographies-run79-wall";
 import * as run107Module from "./character-biographies-run107";
 import * as run111Module from "./character-biographies-run111";
 import * as run114Module from "./character-biographies-run114";
+import * as run126Module from "./character-biographies-run126";
 
 // Existing deep archive-backed bios remain valuable. If a finished long-form narrative
 // has not been written yet, turn the best current deep bio into narrative form rather
@@ -135,6 +136,7 @@ const finishedNarrativeModules = [
   run79WallModule,
   run107Module,
   run114Module,
+  run126Module,
 ] as const;
 
 const richBioSources: Array<Record<string, CharacterBio>> = [
