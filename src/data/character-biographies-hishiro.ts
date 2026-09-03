eb3d6c67b9acf98a67ea285948940255a85b3108
bossmugen <zhenrammsteiner@gmail.com>
2026-09-02 22:03:57 -0700

    wiki: wire Run 282 Hishiro addendum

--- src/data/character-biographies-hishiro.ts
+++ src/data/character-biographies-hishiro.ts
@@ -3,8 +3,10 @@
 // narrative biography, with finished hand-written life stories taking precedence.
 import { allNarrativeCharacterBios } from "./character-biographies-all";
 import { run80NarrativeCharacterBios } from "./character-biographies-run80";
+import { run282HishiroNarrativeCharacterBios } from "./character-biographies-run282-hishiro";
 
 export const hishiroNarrativeCharacterBios = {
   ...allNarrativeCharacterBios,
   ...run80NarrativeCharacterBios,
+  ...run282HishiroNarrativeCharacterBios,
 };
