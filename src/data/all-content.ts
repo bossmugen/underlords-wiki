a824f1dcf5bf6405438c0c5cc282753b60c190ce
bossmugen <zhenrammsteiner@gmail.com>
2026-09-08 14:23:24 -0700

    wiki: activate Run 682 Ricochet deepener

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -17,6 +17,7 @@ import "./integrator-cast-run675-wall";
 import "./integrator-cast-run678-wall";
 import "./integrator-cast-run680-daycare";
 import "./integrator-cast-run680-louvre";
+import "./integrator-cast-run682-daycare";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
