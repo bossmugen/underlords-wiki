80f4c3b22bbaa23394456e25ec186a426075de59
bossmugen <zhenrammsteiner@gmail.com>
2026-09-08 11:37:47 -0700

    wiki: deepen Cele with unread-badge care contradiction

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -13,6 +13,7 @@ import "./integrator-cast-run670-whiskey";
 import "./integrator-cast-run670-core";
 import "./integrator-cast-run671-intake";
 import "./integrator-cast-run674-daycare";
+import "./integrator-cast-run675-wall";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
