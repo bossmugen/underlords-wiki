9deaf1c46d03ddb45d687da57f0f54508c4c5f7f
bossmugen <zhenrammsteiner@gmail.com>
2026-09-09 05:41:54 -0700

    feat(wiki): activate Run 720 intake

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -47,6 +47,7 @@ import "./integrator-cast-run716-daycare-torr";
 import "./integrator-cast-run716-whiskey-spicy";
 import "./integrator-cast-run719-wall-usama";
 import "./integrator-cast-run719-daycare-eos";
+import "./integrator-cast-run720-intake";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
