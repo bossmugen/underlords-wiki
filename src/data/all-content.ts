c057125ba4a8bce2ef87c6347ca818e0e8acc291
bossmugen <zhenrammsteiner@gmail.com>
2026-09-08 20:35:48 -0700

    wiki: activate Run 698 core integration

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -28,6 +28,7 @@ import "./integrator-cast-run692-intake";
 import "./integrator-cast-run694-wall";
 import "./integrator-cast-run694-wall-moon";
 import "./integrator-cast-run697-daycare-wall";
+import "./integrator-cast-run698-core";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
