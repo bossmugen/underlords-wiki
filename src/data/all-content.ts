8b2661693385d93996b0300622f1d07593421ea5
bossmugen <zhenrammsteiner@gmail.com>
2026-09-08 17:14:01 -0700

    wiki: activate Run 689 Jas dossier

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -21,6 +21,7 @@ import "./integrator-cast-run682-daycare";
 import "./integrator-cast-run683-wall";
 import "./integrator-cast-run687-daycare";
 import "./integrator-cast-run688-core-wall";
+import "./integrator-cast-run689-louvre-jas";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
