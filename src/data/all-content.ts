c3a2e2cad50d8a321eca7d2d3f455caa913c0026
bossmugen <zhenrammsteiner@gmail.com>
2026-09-09 02:37:25 -0700

    feat(wiki): activate Run 712 Wall integration

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -41,6 +41,7 @@ import "./integrator-cast-run708-daycare";
 import "./integrator-cast-run708-wall-zhenxi";
 import "./integrator-cast-run708-louvre";
 import "./integrator-cast-run711-daycare";
+import "./integrator-cast-run712-wall";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
