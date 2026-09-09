6dd739f776f872aec61991f9fb37a1fb454aea9c
bossmugen <zhenrammsteiner@gmail.com>
2026-09-08 19:20:23 -0700

    wiki: activate Run 694 Crystalia dossier

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -25,6 +25,7 @@ import "./integrator-cast-run690-wall";
 import "./integrator-cast-run690-wall-aesh";
 import "./integrator-cast-run690-daycare";
 import "./integrator-cast-run692-intake";
+import "./integrator-cast-run694-wall";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
