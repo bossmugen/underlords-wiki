cfc4f7025fcd89ba2ad7e66ba860ebf0a5c9283f
bossmugen <zhenrammsteiner@gmail.com>
2026-09-10 11:46:07 -0700

    feat(wiki): add Cookie JuicyCoochie Petty Crime

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -97,6 +97,7 @@ import "./integrator-identity-repair-run802";
 import "./integrator-cast-run806-wall-yochan";
 import "./integrator-cast-run807-late-wall-momo-mimi";
 import "./integrator-cast-run807-core-daya";
+import "./integrator-cast-run807-core-birthdays";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
