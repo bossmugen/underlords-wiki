eca383cb42e77f377c3248da9ba9e5f4bea3e792
bossmugen <zhenrammsteiner@gmail.com>
2026-09-10 09:55:00 -0700

    wiki: activate Run 802 identity repairs

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -93,6 +93,7 @@ import "./integrator-cast-run794-intake";
 import "./integrator-cast-run798-wall-bishop-daycare-candy";
 import "./integrator-cast-run801-intake";
 import "./integrator-cast-run802-intake";
+import "./integrator-identity-repair-run802";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
