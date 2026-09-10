f264a9378a6d8489958c9d8368cee8ccda3107b8
bossmugen <zhenrammsteiner@gmail.com>
2026-09-10 03:35:33 -0700

    feat(wiki): activate Run 785 Nothien integration

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -82,6 +82,7 @@ import "./integrator-cast-run783-daycare-keon";
 import "./integrator-cast-run783-wall-marsy";
 import "./integrator-cast-run785-daycare-rose";
 import "./integrator-cast-run785-louvre-hishiro";
+import "./integrator-cast-run785-whiskey-nothien";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
