8aff0a692e27bfc3ea4e27de917ce999653af5c5
bossmugen <zhenrammsteiner@gmail.com>
2026-09-10 01:20:24 -0700

    feat(cast): activate Run 780 Woosung correction

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -75,6 +75,7 @@ import "./integrator-cast-run771-core-degen-rat";
 import "./integrator-cast-run775-wall-rookie-cookie-uwu";
 import "./integrator-cast-run777-daycare-woosung";
 import "./integrator-cast-run777-wall-booba";
+import "./integrator-cast-run780-wall-woosung";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
