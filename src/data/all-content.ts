2ad4e88224fb988289f1b1ca4cfe0206868f7f54
bossmugen <zhenrammsteiner@gmail.com>
2026-09-09 19:32:39 -0700

    feat(cast): activate Daya front-door deepening

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -64,6 +64,7 @@ import "./integrator-cast-run749-wall-alkey";
 import "./integrator-cast-run752-intake";
 import "./integrator-cast-run757-daycare-mugen";
 import "./integrator-cast-run760-wall-vanness";
+import "./integrator-cast-run760-daycare-daya";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
