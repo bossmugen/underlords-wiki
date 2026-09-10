12778f47b44c0f6a11694926121b0c5cef3ea694
bossmugen <zhenrammsteiner@gmail.com>
2026-09-10 09:49:22 -0700

    wiki: activate Run 802 cast integration

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -92,6 +92,7 @@ import "./integrator-cast-run791-daycare-gabu";
 import "./integrator-cast-run794-intake";
 import "./integrator-cast-run798-wall-bishop-daycare-candy";
 import "./integrator-cast-run801-intake";
+import "./integrator-cast-run802-intake";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
