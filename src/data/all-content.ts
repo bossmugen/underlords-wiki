00cbca7170af4a80ffa8ead320297f2112f94001
bossmugen <zhenrammsteiner@gmail.com>
2026-09-11 16:18:52 -0700

    feat(wiki): activate Run 890 Effe dossier

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -128,6 +128,7 @@ import "./integrator-cast-run882-louvre-gilli";
 import "./integrator-cast-run886-wall-athena";
 import "./integrator-cast-run886-wall-rummy";
 import "./integrator-cast-run887-core-hishiro-anthos";
+import "./integrator-cast-run890-core-effe";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
