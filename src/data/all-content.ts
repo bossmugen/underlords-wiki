e42c05cdf707edefaf8a9ae16e3561b264ff7a53
bossmugen <zhenrammsteiner@gmail.com>
2026-09-11 15:08:06 -0700

    feat(wiki): activate Run 887 Core person deepener

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -127,6 +127,7 @@ import "./integrator-cast-run881-daycare-rummy";
 import "./integrator-cast-run882-louvre-gilli";
 import "./integrator-cast-run886-wall-athena";
 import "./integrator-cast-run886-wall-rummy";
+import "./integrator-cast-run887-core-hishiro-anthos";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
