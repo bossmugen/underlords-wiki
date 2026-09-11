dc2ebf16f583af774fcff8d1f43550468c179481
bossmugen <zhenrammsteiner@gmail.com>
2026-09-10 22:28:43 -0700

    feat(integrator): activate Run 840 Daycare cast updates

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -105,6 +105,7 @@ import "./integrator-cast-run824-daycare-sarabunny";
 import "./integrator-cast-run826-daycare-nintendo";
 import "./integrator-cast-run826-core-hic-alkey";
 import "./integrator-cast-run836-core-wall";
+import "./integrator-cast-run840-daycare";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
