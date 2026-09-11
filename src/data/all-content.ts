4fb59ec9fdf45434e70c7a7c3781daca2866b0e8
bossmugen <zhenrammsteiner@gmail.com>
2026-09-11 07:03:56 -0700

    feat(integrator): deepen Hishiro receipt-room profile

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -115,6 +115,7 @@ import "./integrator-cast-run860-intake";
 import "./integrator-cast-run860-wall-miihi";
 import "./integrator-cast-run863-wall-erys";
 import "./integrator-cast-run863-wall-aeshleen";
+import "./integrator-cast-run864-wall-hishiro";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
