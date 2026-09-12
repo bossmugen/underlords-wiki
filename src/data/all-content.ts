8a5a9069ab7b0403955b9deb1f93da27bb1e1706
bossmugen <zhenrammsteiner@gmail.com>
2026-09-12 06:37:37 -0700

    feat(wiki): activate Run 931 cast dossiers

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -139,6 +139,7 @@ import "./integrator-cast-run925-intake";
 import "./integrator-cast-run926-wall-daya";
 import "./integrator-cast-run928-daycare-trebach";
 import "./integrator-cast-run928-louvre-wall-cookie-jumanji";
+import "./integrator-cast-run931-daycare-wall-mercy-illien";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
