638f9f9fedc796613cfc4c343a72a4044a41d70c
bossmugen <zhenrammsteiner@gmail.com>
2026-09-11 17:42:49 -0700

    feat(wiki): activate Run 894 Wall micro-profiles

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -130,6 +130,7 @@ import "./integrator-cast-run886-wall-rummy";
 import "./integrator-cast-run887-core-hishiro-anthos";
 import "./integrator-cast-run890-core-effe";
 import "./integrator-cast-run890-wall-bishop";
+import "./integrator-cast-run894-wall-crystalia-support-tech";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
