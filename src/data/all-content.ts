023b5e2e6a4d7829e795d91f48cac01c6ba7928a
bossmugen <zhenrammsteiner@gmail.com>
2026-09-12 05:26:16 -0700

    feat(wiki): load Trebach dossier

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -137,6 +137,7 @@ import "./integrator-cast-run913-birthdays-gilli";
 import "./integrator-cast-run915-whiskey-yumi-ansun";
 import "./integrator-cast-run925-intake";
 import "./integrator-cast-run926-wall-daya";
+import "./integrator-cast-run928-daycare-trebach";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
