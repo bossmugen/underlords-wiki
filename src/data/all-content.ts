4acbeb94bfbde900e156f271618ef0ee1ba6db61
bossmugen <zhenrammsteiner@gmail.com>
2026-09-13 00:41:18 -0700

    feat(cast): activate Run 983 Tae, Rummy, and Usama overlay

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -153,6 +153,7 @@ import "./integrator-cast-run975-wall-torr-gilli";
 import "./integrator-cast-run978-wall-kiro";
 import "./integrator-cast-run979-wall-rotini";
 import "./integrator-cast-run981-louvre-wall-rooks-momo";
+import "./integrator-cast-run983-daycare-wall";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
