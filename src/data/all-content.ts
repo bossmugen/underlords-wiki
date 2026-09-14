e4b519f462d2167484ad79469c6700d6a2ef67f6
bossmugen <zhenrammsteiner@gmail.com>
2026-09-14 13:39:16 -0700

    wiki: activate Sye dossier

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -166,6 +166,7 @@ import "./integrator-cast-run1067-core-daycare";
 import "./integrator-cast-run1071-louvre-qin-mu";
 import "./integrator-cast-run1079-wall-hishiro";
 import "./integrator-cast-run1083-daycare-aj";
+import "./integrator-cast-run1089-daycare-sye";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
