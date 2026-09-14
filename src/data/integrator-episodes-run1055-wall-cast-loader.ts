6479e59039fdde7263a063ac8024b2212bc840e9
bossmugen <zhenrammsteiner@gmail.com>
2026-09-14 01:21:53 -0700

    feat(wiki): load Run 1055 Wall cast dossiers

---
+++ src/data/integrator-episodes-run1055-wall-cast-loader.ts
@@ -0,0 +1,4 @@
+import "./integrator-cast-run1055-wall-feli-spicy";
+
+// Side-effect loader for Run 1055 cast dossiers. The all-content episode glob eagerly loads this module.
+export const episodes = [];
