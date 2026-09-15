21ae76bff038bce8ecae2eebad14ccbbef857029
bossmugen <zhenrammsteiner@gmail.com>
2026-09-15 06:48:33 -0700

    wiki: register WolfPhenix Wall dossier

--- src/data/integrator-cast-run1095-wall-ricochet.ts
+++ src/data/integrator-cast-run1095-wall-ricochet.ts
@@ -4,6 +4,7 @@ import "./integrator-cast-run1110-anthos";
 import "./integrator-cast-run1114-daycare-zyrcant";
 import "./integrator-cast-run1122-daycare-shimoko";
 import "./integrator-cast-run1132-daycare-rosario";
+import "./integrator-cast-run1138-wall-wolfphenix";
 import { allCharacters, characterById } from "./cast";
 import type { Character } from "./wiki";
 
