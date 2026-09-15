8c641cd2890bc514e1a33c18d70a747f65d0ccda
bossmugen <zhenrammsteiner@gmail.com>
2026-09-15 11:28:59 -0700

    feat(cast): activate Run 1153 Daycare overlay

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -170,6 +170,7 @@ import "./integrator-cast-run1089-daycare-sye";
 import "./integrator-cast-run1095-wall-ricochet";
 import "./integrator-cast-run1149-birthdays-sailorluna";
 import "./integrator-cast-run1153-louvre-shiki-dean";
+import "./integrator-cast-run1153-daycare-mimi-new";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
