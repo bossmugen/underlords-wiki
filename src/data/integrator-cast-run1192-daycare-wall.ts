c6587eb8a09ae95d38ca7f9638d050489057e9d6
bossmugen <zhenrammsteiner@gmail.com>
2026-09-16 14:28:11 -0700

    feat(integration): wire Run 1223 Antho dossier

--- src/data/integrator-cast-run1192-daycare-wall.ts
+++ src/data/integrator-cast-run1192-daycare-wall.ts
@@ -6,6 +6,7 @@ import "./integrator-cast-run1203-core-ansun";
 import "./integrator-cast-run1206-core-daycare-wall";
 import "./integrator-cast-run1206-reconcile";
 import "./integrator-cast-run1221-core-daycare-wall";
+import "./integrator-cast-run1223-daycare-antho";
 
 type ExtendedCharacter = Character & {
   claims?: string[];
