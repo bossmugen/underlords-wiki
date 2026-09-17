2abdd4b56a05a69db70409b884ff4636e0e50345
bossmugen <zhenrammsteiner@gmail.com>
2026-09-17 16:47:00 -0700

    wiki: load Run 1294 Nobu and Zhenxi integration

--- src/data/integrator-cast-run1266-wall-mimi.ts
+++ src/data/integrator-cast-run1266-wall-mimi.ts
@@ -1,5 +1,6 @@
 import "./integrator-cast-run1273-wall-torr-hamittey";
 import "./integrator-cast-run1276-wall-panda";
+import "./integrator-cast-run1294-wall-nobu-zhenxi";
 import { allCharacters, characterById } from "./cast";
 import type { Character } from "./wiki";
 
