d9fecdce16a2bb58c16320afc480340f59237776
bossmugen <zhenrammsteiner@gmail.com>
2026-09-18 23:18:16 -0700

    feat(cast): load Celeee dossier

--- src/data/all-content.ts
+++ src/data/all-content.ts
@@ -184,6 +184,7 @@ import "./integrator-cast-run1247-wall-realgamejack";
 import "./integrator-cast-run1266-wall-mimi";
 import "./integrator-cast-run1360-daycare-zoshaa";
 import "./integrator-cast-run1374-wall-ghostt";
+import "./integrator-cast-run1375-whiskey-celeee";
 
 type LooseRecord = Record<string, unknown>;
 type Gag = { id: string; name: string; logline: string };
