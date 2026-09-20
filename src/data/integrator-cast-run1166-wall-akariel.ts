06d96ae29377eb5ffaa6b5a3692123f4f383d7df
bossmugen <zhenrammsteiner@gmail.com>
2026-09-19 22:38:27 -0700

    wiki: wire Run 1445 canonical Rose/DarkAsrai synthesis

--- src/data/integrator-cast-run1166-wall-akariel.ts
+++ src/data/integrator-cast-run1166-wall-akariel.ts
@@ -1,6 +1,7 @@
 import { allCharacters, castGroups, characterById } from "./cast";
 import type { Character } from "./wiki";
 import "./integrator-cast-run1443-whiskey-anayss";
+import "./integrator-cast-run1445-wall-darkasrai";
 
 type ExtendedCharacter = Character & {
   claims?: string[];
