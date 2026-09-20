ced97d6a51812c0912cc0f04e9021c20e76b09bf
bossmugen <zhenrammsteiner@gmail.com>
2026-09-19 22:24:53 -0700

    wiki: wire Run 1443 Anayss synthesis

--- src/data/integrator-cast-run1166-wall-akariel.ts
+++ src/data/integrator-cast-run1166-wall-akariel.ts
@@ -1,5 +1,6 @@
 import { allCharacters, castGroups, characterById } from "./cast";
 import type { Character } from "./wiki";
+import "./integrator-cast-run1443-whiskey-anayss";
 
 type ExtendedCharacter = Character & {
   claims?: string[];
