7d78b8a9a70d46adc4c83b334b67114ee15986c0
bossmugen <zhenrammsteiner@gmail.com>
2026-09-21 08:35:00 -0700

    feat(cast): wire Run 1535 Korea and Nobu synthesis

--- src/data/cast.ts
+++ src/data/cast.ts
@@ -12,6 +12,7 @@ import { applyRun1531WhiskeyCast } from "./cast-run1531-whiskey";
 import { applyRun1531WallShiyaCast } from "./cast-run1531-wall-shiyax";
 import { applyRun1533BirthdaysCast } from "./cast-run1533-birthdays";
 import { applyRun1534WallWooCast } from "./cast-run1534-wall-woo";
+import { applyRun1535IntegratorCast } from "./cast-run1535-integrator";
 import type { Character } from "./wiki";
 export type { CastGroup } from "./cast-through-run1514";
 
@@ -27,6 +28,7 @@ applyRun1531WhiskeyCast(allCharacters);
 applyRun1531WallShiyaCast(allCharacters);
 applyRun1533BirthdaysCast(allCharacters, previousGroups);
 applyRun1534WallWooCast(allCharacters);
+applyRun1535IntegratorCast(allCharacters, previousGroups);
 
 export const castGroups = previousGroups;
 export const characterById = new Map(allCharacters.map((character) => [character.id, character]));
