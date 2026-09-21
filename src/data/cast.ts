08f07520e7d80c7a39babf810635a98833bc0159
bossmugen <zhenrammsteiner@gmail.com>
2026-09-21 14:43:59 -0700

    revert(cast): restore pre-1553 wiring

--- src/data/cast.ts
+++ src/data/cast.ts
@@ -18,7 +18,6 @@ import { applyRun1540 } from "./cast-run1540";
 import { applyRun1540WallCast } from "./cast-run1540-wall";
 import { applyRun1547WallErysCast } from "./cast-run1547-wall-erys";
 import { applyRun1552WhiskeyCast } from "./cast-run1552-whiskey";
-import { applyRun1553IntegratorCast } from "./cast-run1553-integrator";
 import type { Character } from "./wiki";
 export type { CastGroup } from "./cast-through-run1514";
 
@@ -40,7 +39,6 @@ applyRun1540(allCharacters);
 applyRun1540WallCast(allCharacters);
 applyRun1547WallErysCast(allCharacters);
 applyRun1552WhiskeyCast(allCharacters);
-applyRun1553IntegratorCast(allCharacters, previousGroups);
 
 export const castGroups = previousGroups;
 export const characterById = new Map(allCharacters.map((character) => [character.id, character]));
