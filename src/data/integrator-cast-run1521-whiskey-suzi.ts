7eba0402d79a12a205f7af65af7470bef21e2ee5
bossmugen <zhenrammsteiner@gmail.com>
2026-09-21 05:17:46 -0700

    wiki: wire Run 1528 Daycare and Whiskey deepeners

--- src/data/integrator-cast-run1521-whiskey-suzi.ts
+++ src/data/integrator-cast-run1521-whiskey-suzi.ts
@@ -1,6 +1,7 @@
 import "./integrator-cast-run1521-wall-aesh";
 import "./integrator-cast-run1521-daycare-cookie";
 import "./integrator-cast-run1524-daycare-whiskey";
+import "./integrator-cast-run1528-daycare-whiskey";
 import { allCharacters, characterById } from "./cast";
 import type { Character } from "./wiki";
 
