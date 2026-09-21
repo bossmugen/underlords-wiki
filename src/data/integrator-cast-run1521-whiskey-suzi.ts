856f2b503a31156be6b66c270baa058a32b396d8
bossmugen <zhenrammsteiner@gmail.com>
2026-09-21 05:40:35 -0700

    wiki: drop duplicate Run 1529 Plushie wiring

--- src/data/integrator-cast-run1521-whiskey-suzi.ts
+++ src/data/integrator-cast-run1521-whiskey-suzi.ts
@@ -2,7 +2,6 @@ import "./integrator-cast-run1521-wall-aesh";
 import "./integrator-cast-run1521-daycare-cookie";
 import "./integrator-cast-run1524-daycare-whiskey";
 import "./integrator-cast-run1528-daycare-whiskey";
-import "./integrator-cast-run1529-wall-plushie";
 import { allCharacters, characterById } from "./cast";
 import type { Character } from "./wiki";
 
