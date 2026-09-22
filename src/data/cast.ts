5f9247deb2f55774fb7aacadce318b501d8e9a0a
bossmugen <zhenrammsteiner@gmail.com>
2026-09-22 09:33:57 -0700

    wiki: register Run 1604 cast integration

--- src/data/cast.ts
+++ src/data/cast.ts
@@ -38,6 +38,7 @@ import { applyRun1594CoreRicochetCast } from "./cast-run1594-core-ricochet";
 import { applyRun1595WallSpicyHishiroCast } from "./cast-run1595-wall-spicy-hishiro";
 import { applyRun1595WhiskeyDyingFoxCast } from "./cast-run1595-whiskey-dyingfox";
 import { applyRun1603IntegratorCast } from "./cast-run1603-integrator";
+import { applyRun1604WallDaycareCast } from "./cast-run1604-wall-daycare";
 import type { Character } from "./wiki";
 export type { CastGroup } from "./cast-through-run1514";
 
@@ -79,6 +80,7 @@ applyRun1594CoreRicochetCast(allCharacters, previousGroups);
 applyRun1595WallSpicyHishiroCast(allCharacters, previousGroups);
 applyRun1595WhiskeyDyingFoxCast(allCharacters, previousGroups);
 applyRun1603IntegratorCast(allCharacters);
+applyRun1604WallDaycareCast(allCharacters);
 
 export const castGroups = previousGroups;
 export const characterById = new Map(allCharacters.map((character) => [character.id, character]));
